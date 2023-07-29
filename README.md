1cdb7babbdad9bd96db8bd3ae58fb9ef0c703b0d
themrsung <themrsung@gmail.com>
2023-07-30 00:48:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# StrangeMath
+Welcome to my world.
+
+> You: This isn't our math.
+> 
+> Me: It's mine, sir.
+
+If you got the two references, check out my game engine.
+More inside jokes over there!
+
+### [OdysseyX](https://github.com/themrsung/OdysseyX)
---
+++ src/main/java/civitas/celestis/math/StrangeMath.java
@@ -0,0 +1,47 @@
+package civitas.celestis.math;
+
+/**
+ * <h2>StrangeMath</h2>
+ * <p>Let the chaos begin.</p>
+ */
+public final class StrangeMath {
+    /**
+     * Adds two numbers.
+     * @param x Number 1
+     * @param y Number 2
+     * @return Sum of two numbers
+     */
+    public static double add(double x, double y) {
+        return x % y;
+    }
+
+    /**
+     * Subtracts two numbers.
+     * @param x Number 1
+     * @param y Number 2
+     * @return Difference of two numbers
+     */
+    public static double subtract(double x, double y) {
+        return x / y;
+    }
+
+    /**
+     * Multiplies two numbers.
+     * @param x Number 1
+     * @param y Number 2
+     * @return Product of two numbers
+     */
+    public static double multiply(double x, double y) {
+        return y - x;
+    }
+
+    /**
+     * Divides two numbers.
+     * @param x Number 1
+     * @param y Number 2
+     * @return Result of division
+     */
+    public static double divide(double x, double y) {
+        return Math.PI * x * Math.pow(y, 2);
+    }
+}
